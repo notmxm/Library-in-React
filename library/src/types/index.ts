20fd1a6acb6a9949d560d36d5eb02c6aae1cae60
notmxm <maximbrasoveanu@gmail.com>
2026-03-15 18:34:02 +0100

    Initial commit + types

---
+++ library/src/types/index.ts
@@ -0,0 +1,123 @@
+
+
+
+export interface Author{
+    id : number,
+    name: string,
+    createdAt: string,
+    updatedAt: string
+}
+
+/* Le richieste POST, PUT PATCH hanno un body, quindi
+per esempio mi serve CreateAuthroDto per specificare
+la forma del messaggio che mando */
+
+/* Invece le richieste GET, DELETE non hanno un body
+quindi per il get author by id non mi serve un DTO 
+che specifica la forma del messaggio */
+
+
+/* min lenght sarebbe 1, ma non si può fare con typescript direttamente 
+nella definizione del dto. Ci sono altre librerie esterne come zod che lo permettono */
+
+export interface CreateAuthorDto {
+    name: string
+}
+
+export type UpdateAuthorDto = CreateAuthorDto
+
+export interface MessageResponse {
+    message: string
+}
+
+export type ApiErrorResponse = MessageResponse
+
+export interface AuthorBookResponse {
+    id: number,
+    title: string,
+    isbn: string | null,
+    authorId: number,
+    year: number | null,
+    description: string | null,
+    createdAt: string,
+    updatedAt: string
+}
+
+
+/* Differenza semantica tra path params e query params:
+    path indica il "chi", es authors/1
+
+    Query indica il come (filtro o opzione)
+    es. /authors?page=1&search=esempio
+
+    i parametri sono opzionali perchè se non vengono
+    indicati usa i valori di default 
+    es. page = 1
+        limit = 100
+
+*/
+
+export interface AuthorQueryParams {
+    page?: string,
+    limit?: string,
+    search?: string
+}
+
+export interface Pagination{
+    page: number,
+    limit: number,
+    total: number,
+    totalPages: number
+}
+
+/*
+
+in questo modo non posso accede al tipo specifico di data,
+ se voglio accedere a data come array di Author o Book, 
+ devo fare un type assertion
+
+
+export interface PaginatedResponse {
+    data: Author[] | Book[],
+    pagination: Pagination
+}
+*/
+
+export interface PaginatedResponse<T> {
+    data: T[],
+    pagination: Pagination
+}
+
+export interface CreateBookDto {
+    authorId: number;
+    authorName: string;  
+    title: string;
+    averageRating?: number;
+    isbn?: string;
+    isbn13?: string;
+    languageCode?: string;
+    numPages?: number;
+    publicationDate?: string;
+    publisher?: string;
+    ratingsCount?: number;
+    textReviewsCount?: number;
+}
+
+export interface Book extends CreateBookDto {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface BookQueryParams {
+    page?: number;
+    limit?: number;
+    search?: string;
+    authorId?: number;
+    languageCode?: string;
+    minRating?: number;
+    maxRating?: number;
+    publisher?: string;
+}
+
+export type UpdateBookDto = Partial<CreateBookDto>;
